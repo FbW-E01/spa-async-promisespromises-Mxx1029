60ea991e7d9ab0afbcd676b32a2d36fab3b59059
Mxx1029 <lia.pim@icloud.com>
2021-06-24 14:21:46 +0200

    Initial import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,21 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+const p = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        resolve("The modal gets displayed");
+        const modal = document.querySelector(".modal");
+        modal.style.display = "block";
+        modal.addEventListener("click", (e) => {
+        switch(e.target.className) {
+            case "modal__content" :
+            case "close" :
+                modal.style.display = "none";
+                break;
+        };
+    })
+    }, 60000)
+    
+}).then((result) => { console.log("done", result)
+    
+});
